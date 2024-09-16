6b9a8ac5e76e46c34da61ef47805ab6a5ee9dd3c
SnehaPriyanka01 <priyankareddie01@gmail.com>
2024-09-16 15:25:04 +0530

    Add files via upload

---
+++ Chat_models_basic_conversation.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_ollama.chat_models import ChatOllama
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOllama model
+model = ChatOllama(base_url='https://ollama.dealwallet.com/', model='llama3')
+
+# SystemMessage:
+#   Message for priming AI behavior, usually passed in as the first of a sequence of input messages.
+# HumanMessage:
+#   Message from a human to the AI model.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AI: {result.content}")
+
+
+# AIMessage:
+#   Message from an AI.
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+    AIMessage(content="81 divided by 9 is 9."),
+    HumanMessage(content="What is 10 times 5?"),
+]
+
+# Invoke the model with messages
+result = model.invoke(messages)
+print(f"Answer from AIMessage: {result.content}")
